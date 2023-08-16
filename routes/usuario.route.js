4d79d82f594e783eeee3d16cc3ffe63d85172c3d
chrisdresba <chrisdresba@gmail.com>
2023-08-15 22:55:36 -0300

    edit rutas

--- routes/usuario.route.js
+++ routes/usuario.route.js
@@ -12,26 +12,10 @@ const { existEmail, userExistsById } = require("../helpers/db-validators");
 
 const router = Router();
 
-router.get("/", getUsuario);
-
 router.get("/:id", [
   check("id", "No es un ID válido").isMongoId(),
   check("id").custom(userExistsById),
   validateFields,
 ]);
-
-router.post(
-  "/",
-  [
-    check("email", "El correo no es valido").isEmail(),
-    check("email").custom(existEmail),
-    check("nombre", "El nombre es obligatorio").not().isEmpty(),
-    check("contraseña", "El password debe de ser mas de 6 letras").isLength({
-      min: 6,
-    }),
-    validateFields,
-  ],
-  postUsuario
-);
 
 module.exports = router;
